b031ea81c9a8ea2089b95d64b4526128116b04dc
accountapplekrzysia-alt <accountapplekrzysia@icloud.com>
2026-05-28 14:37:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
   });
 });
 
